b718dedb18e9833c8f650fba534bd936aaa023e9
Meraj Niaz Baloch <90909350+merajNizBaloch@users.noreply.github.com>
2026-09-09 23:50:54 +0500

    brand: update social image to uploaded LinkCraft mark

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -33,10 +33,14 @@ export default function OpenGraphImage() {
           </div>
         </div>
 
-        <svg width="300" height="300" viewBox="0 0 512 512" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M151 85V260C151 310 180 339 230 339H360" stroke="#FF5C35" strokeWidth="70" strokeLinecap="round" strokeLinejoin="round" />
-          <path d="M265 164H356C406 164 435 193 435 243V288C435 338 406 367 356 367H282" stroke="#11110F" strokeWidth="70" strokeLinecap="round" strokeLinejoin="round" />
-          <path d="M244 313H298" stroke="#F7F7F4" strokeWidth="20" strokeLinecap="round" />
+        <svg width="330" height="282" viewBox="0 0 472 403" xmlns="http://www.w3.org/2000/svg">
+          <g transform="translate(-202 0)" fill="#11110F" fillRule="evenodd">
+            <path d="M 661 288 L 660 286 L 583 286 L 579 305 L 570 315 L 561 319 L 485 320 L 466 336 L 439 349 L 414 354 L 373 355 L 398 375 L 417 384 L 437 389 L 569 390 L 588 387 L 602 382 L 621 371 L 643 349 L 657 321 L 660 308 Z" />
+            <path d="M 336 241 L 338 243 L 403 242 L 403 196 L 406 183 L 411 174 L 419 165 L 435 156 L 443 154 L 559 154 L 572 160 L 578 167 L 582 177 L 582 195 L 584 197 L 659 197 L 661 195 L 661 174 L 658 156 L 649 134 L 641 122 L 629 109 L 611 96 L 587 86 L 576 84 L 438 84 L 423 87 L 398 97 L 380 109 L 362 127 L 351 143 L 343 160 L 337 182 Z" />
+          </g>
+          <g transform="translate(-202 0)" fill="#FF5C35" fillRule="evenodd">
+            <path d="M 215 13 L 214 254 L 219 278 L 233 304 L 256 325 L 272 333 L 292 338 L 415 338 L 442 331 L 466 317 L 477 307 L 491 288 L 496 278 L 502 258 L 503 206 L 496 190 L 489 183 L 475 176 L 458 176 L 442 184 L 434 194 L 430 204 L 428 247 L 425 253 L 414 263 L 402 266 L 323 266 L 309 262 L 301 255 L 295 241 L 295 50 L 293 42 L 286 29 L 279 22 L 269 16 L 260 13 Z" />
+          </g>
         </svg>
       </div>
     ),
